6ac1704572b4a5a8c3c90f7a7f7d8bd8f11b5cf4
Abhijit Menon <abhijit502@gmail.com>
2018-01-03 13:30:18 +0000

    Update screen.sh

--- screen.sh
+++ screen.sh
@@ -1,36 +1,58 @@
 #! /bin/bash
 
 echo "					        ______________________________________________  "
-echo "					       |					      | "
-echo "					       |  ___ 		        		/\    | "
-echo "					       | |		       		       /  \   | "
-echo "					       | |___	      __ _ _  __  __	      /_  _\  |	"
-echo "					       |     | |_| | |_   |  |_	 |__)	        ||    | "
-echo "					       |  ___| | | | |    |  |__ | \            ||    | "
-echo "					       |______________________________________________| "
-
+echo "					      /|					      | "
+echo "					     / |  ___					/\    | "
+echo "					    |  | |				       /  \   | "
+echo "					    |  | |___	      __ _ _  __  __	      /_  _\  |	"
+echo "					    |  |     | |_| | |_   |  |_	 |__)	        ||    | "
+echo "					    |  |  ___| | | | |    |  |__ | \            ||    | "
+echo "					    |  |______________________________________________| "
+echo "					    | /						     /	"
+echo "					    |/______________________________________________/	"
+echo ""
+echo ""
 
 option=$1
 imagepath=$2
 
-if [ !/boot/grub/grubcopy.cfg ] ; then
+shifter_path="\/usr\/share\/shifter\/image.jpg"
+
+mkdir -p /usr/share/shifter/shifter/
+cp $imagepath /usr/share/shifter/image.jpg
+ 
+if [ ! -f " /boot/grub/grubcopy.cfg " ] ; then
 	cp /boot/grub/grub.cfg /boot/grub/grubcopy.cfg
 
 fi
 
-if [ ' $option ' == " -c " ] || [ ' $option ' == " --change-background " ] ; then
+if [ " $option " = ' -c ' ] || [ " $option " = ' --change-background ' ] ; then
+	if [ -z " $imagepath " ] ; then
+		echo $imagepath
+		echo "No image path specified "
+		exit 2	
+	fi
+	
 
-	line=`grep -n -r "background_image" "/boot/grub/grub.cfg" `
-	lineno= ${line%:*}  #take out the line number from string 
+	line=`grep -n -r "background_image" "/boot/grub/grub.cfg"`
+	lineno=${line%:*}  #take out the line number from string 
+	lineno=$lineno's'
 	linetext=${line##*:} # read the text in the line 
 	firsttxt=${linetext% /*} #read the chars before the image path 
 	lasttxt=${linetext##*;}
-	sed -i "${lineno}s/.*/$firsttxt $imagepath ; $lasttxt/" /boot/grub/grub.cfg
+	sed -i "$lineno/.*/$firsttxt $shifter_path;$lasttxt/" /boot/grub/grub.cfg
 
 
-else if [ ' $option ' == " -h " ] || [ ' $option ' == " --help " ] ; then
+else if [ " $option " = ' -h ' ] || [ " $option " = ' --help ' ] ; then
+ 	
+	echo "Usage - bash ./shifter.sh [option] [imagepath]  "
+	echo "Options-  "
+	echo "-c	--change-backround 	- To change the boot background "
+
+else 
  	
 	echo "Usage - bash ./shifter.sh [option] [imagepath]  "
 
-fi
 
+fi
+fi
